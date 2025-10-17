e41623358f09f1cf882b2a424cd6bafa7338cdf8
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-10-17 11:59:41 +0900

    Implement lex_XXX that parse markdowns into Tags

--- mtoh.go
+++ mtoh.go
@@ -6,11 +6,30 @@ import (
 	"log"
 	"path/filepath"
 	"io/fs"
+	"strings"
 )
 
 type Markdown struct {
 	Filepath string
 	Content  string
+}
+
+type TagType string
+
+const (
+	Header1 TagType = "h1"
+	Header2         = "h2"
+	Header3         = "h3"
+	Header4         = "h4"
+	UnorderedList   = "ul"
+	OrderedList     = "ol"
+	ListItem        = "li"
+	Paragraph       = "p"
+)
+
+type Tag struct {
+	Type    TagType
+	Content string
 }
 
 func read_markdowns(target_dir string) ([]*Markdown, error) {
@@ -46,6 +65,51 @@ func read_markdowns(target_dir string) ([]*Markdown, error) {
 	return out, nil
 }
 
+func lex_line(line string) *Tag {
+	words := strings.Fields(line)
+	if len(words) < 2 {
+		return &Tag{
+			Type: Paragraph, 
+			Content: line,
+		}
+	}
+
+	head := words[0]
+	content := strings.Join(words[1:], "")
+
+	var tagType TagType
+	if head == "#" {
+		tagType = Header1 
+	} else if head == "##" {
+		tagType = Header2
+	} else if head == "###" {
+		tagType = Header3
+	} else if head == "####" {
+		tagType = Header4
+	} else if head == "-" {
+		tagType = ListItem
+	} else {
+		tagType = Paragraph
+	}
+	return &Tag{
+		Type: tagType, 
+		Content: content,
+	}
+}
+
+func lex_markdown(md *Markdown) []*Tag {
+	var out []*Tag
+	lines := strings.Split(md.Content, "\n")
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		ir := lex_line(line)
+		out = append(out, ir)
+	} 
+	return out
+}
+
 func main() {
 	markdowns, err := read_markdowns("./testdata/") 
 	if err != nil {
@@ -53,8 +117,9 @@ func main() {
 	}
 
 	for _, markdown := range markdowns {
-		fmt.Println(markdown.Filepath)
-		fmt.Println(markdown.Content)
+		for _, tag := range lex_markdown(markdown) {
+			fmt.Println(tag.Type, tag.Content)
+		}
 	}
 }
 
